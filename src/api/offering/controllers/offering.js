13d1b0670b9a8853e8e31a442e2d0cb0808505c0
Aaron Brown <41420965+djbrownbear@users.noreply.github.com>
2023-06-06 16:22:41 -0700

    test: custom findOne controller

--- src/api/offering/controllers/offering.js
+++ src/api/offering/controllers/offering.js
@@ -6,4 +6,17 @@
 
 const { createCoreController } = require('@strapi/strapi').factories;
 
-module.exports = createCoreController('api::offering.offering');
+module.exports = createCoreController(
+  "api::offering.offering",
+  ({ strapi }) => ({
+    async findOne(ctx) {
+      const { title } = ctx.params
+      const { results, pagination } = await strapi.db
+        .query("api::offering.offering")
+        .findOne({ where: { slug: title } });
+      const sanitizedResults = await this.sanitizeOutput(results, ctx);
+
+      return this.transformResponse(sanitizedResults, { pagination });
+    },
+  })
+);
--- src/api/offering/routes/offering.js
+++ src/api/offering/routes/offering.js
@@ -6,4 +6,4 @@
 
 const { createCoreRouter } = require('@strapi/strapi').factories;
 
-module.exports = createCoreRouter('api::offering.offering');
+module.exports = createCoreRouter("api::offering.offering");
