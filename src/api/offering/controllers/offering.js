ce33cfe2cfea7e8db8031097b23551a5a9d2e1e8
Aaron Brown <41420965+djbrownbear@users.noreply.github.com>
2023-06-06 16:44:23 -0700

    feat: add slug field to offerings

--- src/api/offering/controllers/offering.js
+++ src/api/offering/controllers/offering.js
@@ -1,19 +1,19 @@
-'use strict';
+"use strict";
 
 /**
  * offering controller
  */
 
-const { createCoreController } = require('@strapi/strapi').factories;
+const { createCoreController } = require("@strapi/strapi").factories;
 
 module.exports = createCoreController(
   "api::offering.offering",
   ({ strapi }) => ({
     async findOne(ctx) {
-      const { title } = ctx.params
+      const { id } = ctx.params;
       const { results, pagination } = await strapi.db
         .query("api::offering.offering")
-        .findOne({ where: { slug: title } });
+        .findOne({ where: { slug: id } });
       const sanitizedResults = await this.sanitizeOutput(results, ctx);
 
       return this.transformResponse(sanitizedResults, { pagination });
