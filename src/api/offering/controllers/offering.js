1dc7472b49a9df8b4e37d1a44779c48ec6d94960
Aaron Brown <41420965+djbrownbear@users.noreply.github.com>
2023-06-06 17:13:48 -0700

    fix: add deep populate

--- src/api/offering/controllers/offering.js
+++ src/api/offering/controllers/offering.js
@@ -6,14 +6,24 @@
 
 const { createCoreController } = require("@strapi/strapi").factories;
 
+const queryPopulate = (populate) => {
+  if (Array.isArray(populate)) {
+    return populate.flatMap(value => {
+      return value.split(',').map(value => value);
+    })
+  }
+}
+
 module.exports = createCoreController(
   "api::offering.offering",
   ({ strapi }) => ({
     async findOne(ctx) {
       const { id } = ctx.params;
+      const { populate } = ctx.query;
+
       const entity = await strapi.db
         .query("api::offering.offering")
-        .findOne({ where: { slug: id } });
+        .findOne({ where: { slug: id }, populate: queryPopulate(populate) });
       const sanitizedResults = await this.sanitizeOutput(entity);
 
       return this.transformResponse(sanitizedResults);
