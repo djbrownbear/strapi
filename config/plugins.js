da532ccddeb848112ec5c5a6cb66c067e11f62b9
Aaron Brown <41420965+djbrownbear@users.noreply.github.com>
2023-10-03 12:32:45 -0700

    feat: upgrade to 4.14.0

--- config/plugins.js
+++ config/plugins.js
@@ -2,6 +2,9 @@ module.exports = ({ env }) => ({
     'users-permissions': {
       config: {
       jwtSecret: env('JWT_SECRET'),
+      jwt: {
+        expiresIn: '7d',
+      }
       },
     },
     upload: {
